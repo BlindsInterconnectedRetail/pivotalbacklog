ff8cd0b6cd8eab4e2abd440406cd96fc996c8366
Tom Cabanski <tom@cabanski.com>
2017-05-06 11:28:50 -0500

    Restructure for lambda

--- src/add/index.js
+++ src/add/index.js
@@ -14,7 +14,6 @@ var myRequest = 'Florida';
 
 
 var Alexa = require('alexa-sdk');
-console.log(process.version);
 
 exports.handler = function (event, context, callback) {
   var alexa = Alexa.handler(event, context);
@@ -28,77 +27,53 @@ exports.handler = function (event, context, callback) {
 
 var handlers = {
   'LaunchRequest': function () {
-    this.emit('Add');
+    console.log('launched backlog');
+    this.emit(':tell', 'You can add stories by saying add');
   },
 
   'Add': function () {
-
-    this.emit(':tell', 'Submitted story');
-    /*
-    httpsGet(myRequest, (myResult) => {
-      console.log("sent     : " + myRequest);
-      console.log("received : " + myResult);
-
-      this.emit(':tell', 'The population of ' + myRequest + ' is ' + myResult);
-
+    console.log('in add');
+    var intentObj = this.event.request.intent;
+    var story = intentObj.slots.Story.value;
+    console.log('raw story: ' + story);
+    if (!story) {
+      story = '';
     }
+    if (story.length > 0) {
+      story = story.substr(story.indexOf(" ") + 1).trim();
+      if (story.toUpperCase() === 'ADD') {
+        story = '';
+      }
+    }
+    console.log('normalized story: ' + story);
+    if (!story) {
+      var slotToElicit = 'Story';
+      var speechOutput = 'What story would you like to add?';
+      var repromptSpeech = speechOutput;
+      var updatedIntent = this.event.request.intent;
+      console.log("attempting to elicit story");
+      try {
+        this.emit(':elicitSlot', slotToElicit, speechOutput, repromptSpeech, updatedIntent);
+        console.log('elicit slot worked');
+      } catch (err) {
+        console.log("error elicit slot " + err);
+      }
+
+    } else {
+      console.log('attempting to tell story');
+      this.emit(':tell', 'My story is really ' + story);
+    }
+
+    /*
+    httpsGet(myRequest,  (myResult) => {
+            console.log("sent     : " + myRequest);
+            console.log("received : " + myResult);
+
+            this.emit(':tell', 'The population of ' + myRequest + ' is ' + myResult );
+
+        }
     );
     */
 
   }
 };
-
-
-//    END of Intent Handlers {} ========================================================================================
-// 3. Helper Function  =================================================================================================
-
-/*
-var https = require('https');
-// https is a default part of Node.JS.  Read the developer doc:  https://nodejs.org/api/https.html
-// try other APIs such as the current bitcoin price : https://btc-e.com/api/2/btc_usd/ticker  returns ticker.last
-
-function httpsGet(myData, callback) {
-
-  // GET is a web service request that is fully defined by a URL string
-  // Try GET in your browser:
-  // https://cp6gckjt97.execute-api.us-east-1.amazonaws.com/prod/stateresource?usstate=New%20Jersey
-
-
-  // Update these options with the details of the web service you would like to call
-  var options = {
-    host: 'cp6gckjt97.execute-api.us-east-1.amazonaws.com',
-    port: 443,
-    path: '/prod/stateresource?usstate=' + encodeURIComponent(myData),
-    method: 'GET',
-
-    // if x509 certs are required:
-    // key: fs.readFileSync('certs/my-key.pem'),
-    // cert: fs.readFileSync('certs/my-cert.pem')
-  };
-
-  var req = https.request(options, res => {
-    res.setEncoding('utf8');
-    var returnData = "";
-
-    res.on('data', chunk => {
-      returnData = returnData + chunk;
-    });
-
-    res.on('end', () => {
-      // we have now received the raw return data in the returnData variable.
-      // We can see it in the log output via:
-      // console.log(JSON.stringify(returnData))
-      // we may need to parse through it to extract the needed data
-
-      var pop = JSON.parse(returnData).population;
-
-      callback(pop);  // this will execute whatever function the caller defined, with one argument
-
-    });
-
-  });
-  req.end();
-  
-
-}
-*/
